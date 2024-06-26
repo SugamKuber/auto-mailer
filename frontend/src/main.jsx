d363a34483d07bc41f21607d599b8da6414ca1d8
sugam kuber <sugam7842692@gmail.com>
2024-06-26 15:33:39 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,33 +1,27 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Root from "./pages/root";
 import ErrorPage from "./pages/error-page";
 import Check from "./pages/check";
 import Failed from "./pages/failed";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/check",
-    element: <Check />,
-  },
-  {
-    path: "/failed",
-    element: <Failed />,
-  },
-]);
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/check" element={<Check />} />
+        <Route path="/failed" element={<Failed />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
